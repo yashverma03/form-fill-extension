bb04435102006592ce68aa7335cb35cb4c60da54
yashverma03 <vermayash2003@gmail.com>
2026-06-26 15:47:36 +0530

    chore: update package dependencies and re-order Vite plugins for improved configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.config';
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [crx({ manifest }), react()],
 });
